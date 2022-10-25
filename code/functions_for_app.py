119b81bc0d5de845b9f74a24038a5f6bdf434e14
Julian Gjestvang <jg@applica.no>
2022-10-25 14:53:01 +0200

    make script for Montecarlo

--- code/functions_for_app.py
+++ code/functions_for_app.py
@@ -1,5 +1,6 @@
 #Import packages
 import random
+import matplotlib.pyplot as plt
 
 #Make two dices function
 def roll_dice():
@@ -12,3 +13,41 @@ def roll_dice():
     else:
         same_num = False
     return same_num
+
+#Betting function two dices
+def bettin_montecarlo_sim(number_of_simulations,max_number_of_rolls,bet_per_roll,start_balance,odds_per_roll):
+    num_simulations = number_of_simulations
+    max_num_rolls = max_number_of_rolls
+    bet = bet_per_roll
+    win_probability = []
+    end_balance = []
+
+    for i in range(num_simulations):
+        balance = [start_balance]
+        num_rolls = [0]
+        num_wins = 0
+        while num_rolls[-1] < max_num_rolls:
+            same = roll_dice()
+            if same:
+                balance.append(balance[-1] + odds_per_roll * bet_per_roll)
+                num_wins += 1
+            else:
+                balance.append(balance[-1] - bet_per_roll)
+            num_rolls.append(num_rolls[-1] + 1)
+        win_probability.append(num_wins / num_rolls[-1])
+        end_balance.append(balance[-1])
+        plt.plot(num_rolls, balance)
+    plt.title("Monte Carlo Dice Game [" + str(num_simulations) + " simulations]")
+    plt.xlabel("Roll Number")
+    plt.ylabel("Balance [$]")
+    plt.xlim([0, max_num_rolls])
+    plt.show()
+
+    overall_win_probability = sum(win_probability) / len(win_probability)
+    overall_end_balance = sum(end_balance) / len(end_balance)
+    print("Average win probability after " + str(num_simulations) + "   runs: " + str(overall_win_probability))
+    print("Average ending balance after " + str(num_simulations) + " runs: $" + str(overall_end_balance))
+
+
+bettin_montecarlo_sim(100,100,1,100,0.2)
+#%%
--- code/streamlite_example3.py
+++ code/streamlite_example3.py
@@ -1,49 +1,86 @@
 #Import the streamlit lib
 import streamlit as st
 import random
-#Tittel for app med header, markdown og eksempel
-st.title("Montecarlo Simulator")
+import matplotlib.pyplot as plt
 
-st.header("sdsd")
+#Layout
+col1,col2,col3 = st.columns([1,1,1], gap="medium")
 
-st.markdown("### sdsd")
 
-st.write("""
-        sdsdsd :
 
-""")
+#%%
+#COL1
+col1.markdown("# Monte Carlo Simulation")
+col1.markdown("Here is some info")
 
-st.latex(r'''
-    BMI = \frac{vekt}{hoyde^2}
-    ''')
 
-st.markdown("### Eksempel")
 
-st.write("Per er 1.90 meter høy og veier 90 kg. BMIen til Per er:")
 
-st.latex(r'''
-    BMI = \frac{90}{(1.9)^2} = 24.93
-    ''')
-#Math
+#%%
+#COL 2
+#Functions
 def roll_dice():
     die_1 = random.randint(1,6)
     die_2 = random.randint(1,6)
-
     if die_1 == die_2:
         same_num = True
 
     else:
         same_num = False
+
     return die_1, die_2, same_num
+#Layout
+col2.header(" Roll dices")
 
-die_1, die_2, same_num = roll_dice()
+if col2.checkbox("Roll two dices"):
+    die_1, die_2, same_num = roll_dice()
+    if(same_num==True):
+        col2.success("Success!")
+        col2.text("Die 1 : {}.".format(die_1))
+        col2.text("Die 2 : {}.".format(die_2))
 
-st.write("Die 1: ", die_1)
-st.write("Die 2: ", die_2)
-st.write("Same?")
-if(same_num==True):
-    st.success("Success")
-elif(same_num==False):
-    st.warning("Fail")
+    else:
+        col2.error("Not success!")
+        col2.text("Die 1 : {}.".format(die_1))
+        col2.text("Die 2 : {}.".format(die_2))
+
+
+
+#%%
+#COL 3
+#Functions
+
+def bettin_montecarlo_sim(number_of_simulations,max_number_of_rolls,bet_per_roll,start_balance,odds_per_roll):
+    num_simulations = number_of_simulations
+    max_num_rolls = max_number_of_rolls
+    bet = bet_per_roll
+    win_probability = []
+    end_balance = []
+    fig = plt.figure()
+    for i in range(num_simulations):
+        balance = [start_balance]
+        num_rolls = [0]
+        num_wins = 0
+        while num_rolls[-1] < max_num_rolls:
+            same = roll_dice()
+            if same:
+                balance.append(balance[-1] + odds_per_roll * bet_per_roll)
+                num_wins += 1
+            else:
+                balance.append(balance[-1] - bet_per_roll)
+            num_rolls.append(num_rolls[-1] + 1)
+        win_probability.append(num_wins / num_rolls[-1])
+        end_balance.append(balance[-1])
+        plt.plot(num_rolls, balance)
+    plt.title("Monte Carlo Dice Game [" + str(num_simulations) + " simulations]")
+    plt.xlabel("Roll Number")
+    plt.ylabel("Balance [$]")
+    plt.xlim([0, max_num_rolls])
+    col3.pyplot(fig)
+
+
+if col3.checkbox("Monte"):
+    bettin_montecarlo_sim(100,100,2,100,0.2)
+
 
 #%%
